4e4d77ab0a3ca7ae4c0545c62dad64ece68bc7d3
unknown <RICHBAK@CILABS-119A-12.scilsnet.rutgers.edu>
2015-09-17 20:55:34 -0400

    Added to Repo

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardcount;
+	public float startwait;
+	public float spawnwait;
+	private int wavewait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+	void Start(){
+
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update (){
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+	
+		IEnumerator SpawnWaves(){
+		int wavewait = 3;
+		while (true) {
+			yield return new WaitForSeconds (startwait);
+			for (int i = 0; i < hazardcount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnwait);
+
+			}
+			yield return new WaitForSeconds (wavewait);
+
+			if (gameOver){
+				restartText.text = "Press 'R' to Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void Addscore(int newScoreValue){
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	void UpdateScore(){
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver(){
+		gameOverText.text = "Git Gud";
+		gameOver = true;
+
+	}
+}
---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class Player_controller : MonoBehaviour {
+
+	public float speed;
+	public float tilt; 
+	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotspawn;
+	public float fireRate;
+	private float nextFire;
+	void Update() {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotspawn.position, shotspawn.rotation);
+		}
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement*speed;
+		rigidbody.position = new Vector3 (Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax));
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+	
+
+}
